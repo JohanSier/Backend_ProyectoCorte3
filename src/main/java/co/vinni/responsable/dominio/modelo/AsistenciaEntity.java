2c123fa84c70ea90b804643fc07d1f4b5ce47ab1
Johan Gomez <jgomezm11@ucentral.edu.co>
2026-04-28 21:30:21 -0500

    HU003 Initial version asistencia

---
+++ src/main/java/co/vinni/responsable/dominio/modelo/AsistenciaEntity.java
@@ -0,0 +1,25 @@
+package co.vinni.responsable.dominio.modelo;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Entity
+@Table(name = "ASISTENCIA")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AsistenciaEntity extends PanacheEntity {
+
+    public Long id_asistencia;
+    public String asistente_id;
+    public Date fecha;
+    public String tipo_servicio;
+    public String observaciones;
+    public Date fecha_creacion;
+
+}
